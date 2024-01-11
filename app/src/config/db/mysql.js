e7afd9a0ace0cb192910c90b8674f3a64ea41204
junest66 <chlwndks333@gmail.com>
2024-01-11 23:31:38 +0900

    refactor: user 테이블 컬럼 설정 값 변경
    #10

--- app/src/config/db/mysql.js
+++ app/src/config/db/mysql.js
@@ -12,34 +12,3 @@ const db = mysql.createPool({
 });
 
 module.exports = db;
-
-// const testDatabaseConnection = async () => { // db test
-//     const db = mysql.createPool({
-//         host: process.env.DATABASE_HOST,
-//         user: process.env.DATABASE_USERNAME,
-//         password: process.env.DATABASE_ROOT_PASSWORD,
-//         database: process.env.DATABASE_NAME,
-//         port: process.env.DATABASE_PORT,
-//         connectionLimit: 5,
-//     });
-
-//     try {
-//         const connection = await db.getConnection();
-//         console.log("Connected to the database!");
-
-//         const [rows, fields] = await connection.execute("SHOW TABLES");
-
-//         console.log("Tables in the database:");
-//         rows.forEach((row) => {
-//             console.log(row[`Tables_in_${process.env.DATABASE_NAME}`]);
-//         });
-
-//         connection.release();
-//     } catch (error) {
-//         console.error("Error connecting to the database:", error.message);
-//     } finally {
-//         await db.end();
-//     }
-// };
-
-// testDatabaseConnection();
