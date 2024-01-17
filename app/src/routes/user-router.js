29bce6a580ce0da5a445e038157c9819be5117a6
junest66 <chlwndks333@gmail.com>
2024-01-18 01:27:29 +0900

    style: 불필요한 줄바꿈제거

--- app/src/routes/user-router.js
+++ app/src/routes/user-router.js
@@ -6,7 +6,6 @@ const router = express.Router();
 const ctrl = require('../controllers/user-controller');
 const userValidation = require('../middlewares/user-validation');
 
-
 router.get("/emails", userValidation.validateEmail, ctrl.checkEmailDuplication);
 router.post("/", userValidation.validateSignup, ctrl.register);
 router.post("/login", ctrl.login);
