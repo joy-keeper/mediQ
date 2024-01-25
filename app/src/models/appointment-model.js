bb114f5eb7089d6e505d2af4c51c171866b42cde
junest66 <chlwndks333@gmail.com>
2024-01-25 14:28:53 +0900

    feat: appointment 업데이트쿼리, Id로 찾는 쿼리 추가
    #21

--- app/src/models/appointment-model.js
+++ app/src/models/appointment-model.js
@@ -1,20 +1,37 @@
 "use strict"
 
 const pool = require('../config/db/mysql');
+const { convertToCamelCase } = require('../utils/util');
+
+async function findAppointmentById(appointmentId) {
+    const query = `SELECT * FROM appointment WHERE id = ?`;
+    const [rows] = await pool.execute(query, [appointmentId]);
+    return rows.length > 0 ? convertToCamelCase(rows[0]) : null;
+}
 
 async function findAppointmentByUserIdAndSlotId(userId, scheduleSlotId) {
-    const query = `SELECT * FROM appointment WHERE user_id = ? AND schedule_slot_id = ? AND status = '예약완료'`;
+    const query = `SELECT * FROM appointment WHERE user_id = ? AND schedule_slot_id = ? AND status = 'confirmed'`;
     const [rows] = await pool.execute(query, [userId, scheduleSlotId]);
     return rows.length > 0;
 }
 
-async function insertAppoint(userId, appointmentNumber, appointmentDTO, conn) {
+async function insertAppointment(userId, appointmentNumber, appointmentDTO, conn) {
     const query = `INSERT INTO appointment (user_id, schedule_slot_id, appointment_number, notes, type) VALUES (?, ?, ?, ?, ?)`;
     const params = [userId, appointmentDTO.scheduleSlotId, appointmentNumber, appointmentDTO.notes, appointmentDTO.type];
     await conn.execute(query, params);
 }
 
+async function updateAppointment(appointmentId, updateData) {
+    const keys = Object.keys(updateData);
+    const values = Object.values(updateData);
+    const updateFields = keys.map(key => `${key} = ?`).join(', ');
+    const sql = `UPDATE appointment SET ${updateFields} WHERE id = ?`;
+    await pool.execute(sql, [...values, appointmentId]);
+}
+
 module.exports = {
+    findAppointmentById,
     findAppointmentByUserIdAndSlotId,
-    insertAppoint,
+    insertAppointment,
+    updateAppointment,
 };
