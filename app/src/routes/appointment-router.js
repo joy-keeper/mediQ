0acdac482480816a4ac074205c2de547a3bedd10
junest66 <chlwndks333@gmail.com>
2024-02-01 16:22:50 +0900

    feat: 예약 상태 변경 라우터 등록
    #23

--- app/src/routes/appointment-router.js
+++ app/src/routes/appointment-router.js
@@ -8,5 +8,6 @@ const auth = require('../middlewares/auth-middleware');
 
 router.post("/", auth.isAuth, ctrl.makeAppointment);
 router.patch("/:appointmentId", auth.isAuth, ctrl.modifyAppointment);
+router.patch("/:appointmentId/status", auth.isAuth, ctrl.modifyAppointmentStatus);
 
 module.exports = router;
