343edd0691e65793c5b9170b9e81cd9a79445714
junest66 <chlwndks333@gmail.com>
2024-02-13 23:51:44 +0900

    refactor: max_appointments 컬럼 수정
    #29

--- app/src/config/db/migration-scripts/V4_DeleteScheduleSlotColumn.sql
+++ app/src/config/db/migration-scripts/V4_DeleteScheduleSlotColumn.sql
@@ -3,3 +3,9 @@ DROP COLUMN current_appointments;
 
 ALTER TABLE schedule_slot
 DROP COLUMN next_appointment_number;
+
+ALTER TABLE medical_schedule
+CHANGE COLUMN max_appointments initial_max_appointments INTEGER NOT NULL;
+
+ALTER TABLE schedule_slot
+ADD COLUMN slot_max_appointments INTEGER NOT NULL;
